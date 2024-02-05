75e985311a9e37efeb03b5f4d2274d4eb49deb7a
Ping Wang <wangping@westlake.edu.cn>
2024-02-05 16:19:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,30 +5,13 @@ import os.path as osp
 import scipy.io as scio
 import numpy as np 
 import einops
-from utils import save_image, compare_ssim, compare_psnr, TestData
+from utils import Logger, load_checkpoint, save_image, compare_ssim, compare_psnr, TestData
 from opts import parse_args
-from models.network import E2E_SCI
-from utils import Logger, load_checkpoint, load_checkpoint_128_block_256, load_checkpoint_128_expand_256,save_single_image
+from model.joint_optimization import DeepOpticsSCI
 import time
 import cv2
 
 
-
-def save_single_image(images,image_dir,batch,name="",demosaic=False):
-    images = images*255
-    if len(images.shape)==4:
-        frames = images.shape[1]
-    else:
-        frames = images.shape[0]
-    for i in range(frames):
-        begin_frame = batch*frames
-        if len(images.shape)==4:
-            single_image = images[:,i].transpose(1,2,0)[:,:,::-1]
-        else:
-            single_image = images[i]
-        if demosaic:
-            single_image = demosaicing_bayer(single_image,pattern='BGGR')
-        cv2.imwrite(osp.join(image_dir,name+"_"+str(begin_frame+i+1)+".png"),single_image)
 def test(args, network, logger, test_dir, writer=None, epoch=1):
     network = network.eval()
     test_data = TestData(args) 
@@ -45,9 +28,7 @@ def test(args, network, logger, test_dir, writer=None, epoch=1):
         gt = gt[0].float().numpy()
         batch_size,frames,height,width = gt.shape
         with torch.no_grad():
-            # gt_temp = einops.rearrange(gt,"b f h w-> (b f) h w")
             out_pic_list = network(torch.from_numpy(gt).to(args.device))
-        # out_pic_list = network(meas)
         out_pic = out_pic_list[-1].cpu().numpy()
         psnr_t = 0
         ssim_t = 0
@@ -66,12 +47,6 @@ def test(args, network, logger, test_dir, writer=None, epoch=1):
         out_list.append(out_pic)
         gt_list.append(gt)
         
-    # all_out = np.array(all_out)
-    # all_gt= np.array(all_gt)
-    # print(all_out.shape)
-    # print(all_gt.shape)
-    # out_gt = np.stack([all_out,all_gt],axis=0)
-    # scio.savemat("/home/wangping/codes/iccv2023/all_mat/res.mat",{"res":out_gt})
 
     for i,name in enumerate(test_data.data_list):
         _name,_ = name.split("_")
@@ -82,10 +57,6 @@ def test(args, network, logger, test_dir, writer=None, epoch=1):
         for j in range(out.shape[0]):
             image_name = os.path.join(test_dir,"epoch_"+str(epoch)+"_"+_name+"_"+str(j)+".png")
             save_image(out[j],gt[j],image_name)
-            # image_dir = os.path.join("/home/wangping/codes/iccv2023/stformer_16_epoch_280",_name)
-            # if not os.path.exists(image_dir):
-            #     os.makedirs(image_dir)
-            # save_single_image(out[j],image_dir,j,_name)
     if writer is not None:
         writer.add_scalar("psnr_mean",np.mean(psnr_list),epoch)
         writer.add_scalar("ssim_mean",np.mean(ssim_list),epoch)
@@ -96,37 +67,15 @@ def test(args, network, logger, test_dir, writer=None, epoch=1):
 
 if __name__=="__main__":
     args = parse_args()
-    os.environ["CUDA_VISIBLE_DEVICES"] = "8"
-    # args.test_weight_path = '/home/wangping/codes/iccv2023/weights/E2E-STFormer-16graylevels/2023_02_20_14_30_10/epoch_100.pth'
-    # args.test_weight_path = '/home/wangping/codes/iccv2023/weights/E2E-UnetSCI-16graylevels/2023_02_21_21_34_08/epoch_200.pth'
-
-    # args.test_weight_path = '/home/wangping/codes/iccv2023/checkpoints/stformer_256_cr_10_epoch_138.pth'
-    # args.decoder_type = 'STFormer'
-    # args.test_weight_path = '/home/wangping/codes/iccv2023/checkpoints/final_simulation/stformer_16_epoch_280.pth'
-    args.decoder_type = 'Res2former'
-    args.test_weight_path = '/home/wangping/codes/iccv2023/checkpoints/final_simulation/unetsci_16_epoch_342.pth'
-    # args.decoder_type = 'UnetSCI_L'
-    # args.test_weight_path = '/home/wangping/codes/iccv2023/checkpoints/final_simulation/unetsci_large_16_epoch_261.pth'
-    # args.benchmark_path = '/home/wangping/datasets/SCI/simulation'
-    # args.benchmark_path = '/home/wangping/datasets/SCI/simulation1'
-
-    args.size = [256,256]
-    args.train_type = 'sim'
-    args.B = 8
-
     test_path = "test_results"
-    network = E2E_SCI(args).to(args.device)
+    network = DeepOpticsSCI(args).to(args.device)
     log_dir = os.path.join("test_results","log")
     if not os.path.exists(log_dir):
         os.makedirs(log_dir,exist_ok=True)
     logger = Logger(log_dir)
     if args.test_weight_path is not None:
         pretrained_dict = torch.load(args.test_weight_path)
-        load_checkpoint(network, pretrained_dict)
-        # load_checkpoint_128_block_256(network, pretrained_dict)
-        if 'pretrain_epoch' in pretrained_dict.keys():
-            pretrain_epoch = pretrained_dict['pretrain_epoch']
-            logger.info('Pretrain epoch: {}'.format(pretrain_epoch))     
+        load_checkpoint(network, pretrained_dict) 
     else:
         raise ValueError('Please input a weight path for testing.')
 
